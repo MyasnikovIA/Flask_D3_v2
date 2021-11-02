16f6acab2f174bf8f27524f32028212deb49cde0
MyasnikovIA <Miacom@mail.ru>
2021-11-02 10:50:41 +0700

    Поправил создание commit

--- commit_images.bat
+++ commit_images.bat
@@ -1,9 +1,8 @@
-set MIS_DOCKER_NAME=python3.6_flask_001
-set MIS_DOCKER_IMAGE="python3.6_flask:flask_001"
-set MIS_DOCKER_HOSTNAME=PYTHON_3.6_FLASK_001
+set MIS_DOCKER_NAME=python3.6_d3ext
+set MIS_DOCKER_IMAGE="python3.6_flask:d3ext"
+set MIS_DOCKER_HOSTNAME=PYTHON_3.6_D3EXT
 set MIS_ROOT=%~dp0
 set datestr=%date%
-
 for /f %%i in ('docker ps -aqf "name=%MIS_DOCKER_NAME%"') do If not "%%i" == "" (
 	docker commit %%i %MIS_DOCKER_IMAGE%
   ) else (
