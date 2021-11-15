53fbed1baf7ab87f6712fad644e8dc5178a5edda
MyasnikovIA <Miacom@mail.ru>
2021-11-15 16:20:40 +0700

    Правка Android клиента

--- android/D3ExtClient/app/src/main/java/ru/miacomsoft/d3extclient/MainActivity.java
+++ android/D3ExtClient/app/src/main/java/ru/miacomsoft/d3extclient/MainActivity.java
@@ -1,21 +1,19 @@
 package ru.miacomsoft.d3extclient;
 
 import android.annotation.SuppressLint;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import ru.miacomsoft.d3extclient.Lib.Android;
 import ru.miacomsoft.d3extclient.Lib.UserWebChromeClient;
 import ru.miacomsoft.d3extclient.Lib.UserWebClient;
@@ -24,27 +22,18 @@ public class MainActivity extends AppCompatActivity {
     public static WebView webView ;
     private Button button;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        webView = new WebView(this);
+        button = (Button) findViewById(R.id.button);
         String urlText = loadConfig();
         if (urlText.length()==0){
+            setContentView(R.layout.activity_main);
             urlText ="http://128.0.24.172:9091";
             saveConfig(urlText);
-            button = (Button) findViewById(R.id.button);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    EditText infoEdit = (EditText) findViewById(R.id.editText);
-                    String urlText = infoEdit.toString();
-                    saveConfig(urlText);
-                    runWebClient(urlText);
-                }
-            });
         }else{
             runWebClient(urlText);
         }
@@ -52,50 +41,59 @@ public class MainActivity extends AppCompatActivity {
         // webView.reload();
     }
 
-    public void runWebClient(String urlText){
-        webView = new WebView(this);
+    public void onClickButton(View v) {
+        EditText infoEdit = (EditText) findViewById(R.id.editText);
+        String urlText = infoEdit.getText().toString();
+        saveConfig(urlText);
+        runWebClient(urlText);
+    }
+
+    public void runWebClient(String urlText) {
+        setContentView(webView);
         webView.setWebViewClient(new UserWebClient());
         webView.setWebChromeClient(new UserWebChromeClient());
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);  // Включить обработчик JS
         webView.addJavascriptInterface(new Android(this,webView), "Android");
-        setContentView(webView);
         webView.loadUrl(urlText);
     }
 
     @Override
     public void onBackPressed() {
-        //super.onBackPressed();
-        webView.reload();
+        // super.onBackPressed();
+        // webView.reload(); // перезагрузить браузер
+        setContentView(R.layout.activity_main); //  запуск конфигурации для подключения
+        EditText infoEdit = (EditText) findViewById(R.id.editText);
+        infoEdit.setText(loadConfig());
     }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public static String loadConfig() {
-        String fileName="config.ini";
-        File file = new File (fileName);
-        if (!file.exists()){
-            return "";
-        }
-        String everything = "";
+    public void saveConfig(String str) {
         try {
-            everything = new String(Files.readAllBytes(Paths.get(fileName)));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput("config.ini", MODE_PRIVATE)));
+            bw.write(str);
+            bw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return everything;
     }
 
-    public static void saveConfig(String str) {
-        String fileName="config.ini";
-        BufferedWriter writer = null;
+    public String loadConfig() {
+        StringBuffer res= new StringBuffer();
         try {
-            writer = new BufferedWriter(new FileWriter(fileName));
-            writer.write(str);
-            writer.close();
+            BufferedReader br = new BufferedReader(new InputStreamReader( openFileInput("config.ini")));
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                res.append(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return res.toString();
     }
 
 }
